8594655af7b015bf3e6da5ad30697c465b84cb7f
Chris Thunes <cthunes@brewtab.com>
2020-09-20 12:34:28 -0400

    util: Add DataSize parsing support

--- src/main/java/net/qrono/server/util/DataSize.java
+++ src/main/java/net/qrono/server/util/DataSize.java
@@ -1,10 +1,14 @@
 package net.qrono.server.util;
 
+import com.google.common.base.Preconditions;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DataSize {
+  private static final Pattern PARSER = Pattern.compile("(\\d+)\\s*([a-zA-Z]*)$");
   private final long bytes;
 
   private DataSize(long bytes) {
@@ -43,6 +47,35 @@ public class DataSize {
   @Override
   public int hashCode() {
     return Objects.hash(bytes);
+  }
+
+  public static DataSize fromString(String value) {
+    Matcher matcher = PARSER.matcher(value);
+    Preconditions.checkArgument(matcher.matches(), "unsupported format");
+    long base = Long.parseLong(matcher.group(1));
+    long unit = parseUnit(matcher.group(2).strip());
+    return fromBytes(base * unit);
+  }
+
+  private static long parseUnit(String unit) {
+    if (unit.isEmpty() || unit.equals("B")) {
+      return 1;
+    }
+    if (unit.endsWith("B") && unit.length() == 2) {
+      switch (Character.toLowerCase(unit.charAt(0))) {
+        case 'k':
+          return 1024L;
+        case 'm':
+          return 1024L * 1024;
+        case 'g':
+          return 1024L * 1024 * 1024;
+        case 't':
+          return 1024L * 1024 * 1024 * 1024;
+        case 'p':
+          return 1024L * 1024 * 1024 * 1024 * 1024;
+      }
+    }
+    throw new IllegalArgumentException("unknown unit: " + unit);
   }
 
   public static DataSize fromBytes(long bytes) {
