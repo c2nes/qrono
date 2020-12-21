009568371a2c8614ae6c11a794fd8ebeb24e09ac
Chris Thunes <cthunes@brewtab.com>
2020-12-21 12:29:59 -0500

    qrono-bench: Move to tokio

--- qrono-bench/src/main.rs
+++ qrono-bench/src/main.rs
@@ -1,36 +1,37 @@
-use async_std::task::JoinHandle;
-use async_std::{channel, task};
 use futures::stream::StreamExt;
-use redis::RedisResult;
+use tokio::task::JoinHandle;
 
 async fn bench() -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1:16379/")?;
     let conn = client.get_multiplexed_async_connection().await?;
-    let (s, r) = channel::bounded::<JoinHandle<RedisResult<(i64, i64)>>>(10);
-    let complete = task::spawn(r.for_each(|handle| async {
+    let (s, r) = tokio::sync::mpsc::channel::<JoinHandle<()>>(10);
+    let complete = tokio::spawn(r.for_each(|handle| async {
         handle.await.unwrap();
     }));
 
+    let s = s;
     for _ in 0..1_000_000 {
         let mut conn = conn.clone();
-        let handle = task::spawn(async move {
+        let handle = tokio::spawn(async move {
             redis::cmd("ENQUEUE")
                 .arg("my-queue")
                 .arg("my-value-123")
                 .query_async::<_, (i64, i64)>(&mut conn)
                 .await
+                .unwrap();
         });
 
         s.send(handle).await.expect("channel should not be closed")
     }
 
-    s.close();
-    complete.await;
+    drop(s);
+    complete.await.unwrap();
 
     Ok(())
 }
 
-fn main() {
-    async_std::task::block_on(bench()).unwrap();
+#[tokio::main]
+async fn main() {
+    bench().await.unwrap();
     println!("Done!");
 }
