20dde120aa2a36d93376ccfd4a51505b2b7025df
Chris Thunes <cthunes@brewtab.com>
2020-12-24 11:46:27 -0500

    qrono-bench: Reimplement and improve throughput

--- qrono-bench/src/main.rs
+++ qrono-bench/src/main.rs
@@ -1,31 +1,42 @@
+use std::net::SocketAddr;
+
+use bytes::BytesMut;
 use futures::stream::StreamExt;
-use tokio::task::JoinHandle;
+use redis_async::resp::RespCodec;
+use redis_async::resp_array;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+use tokio_util::codec::{Encoder, FramedRead};
 
-async fn bench() -> redis::RedisResult<()> {
-    let client = redis::Client::open("redis://127.0.0.1:16379/")?;
-    let conn = client.get_multiplexed_async_connection().await?;
-    let (s, r) = tokio::sync::mpsc::channel::<JoinHandle<()>>(10);
-    let complete = tokio::spawn(r.for_each(|handle| async {
-        handle.await.unwrap();
-    }));
+async fn bench() -> Result<(), std::io::Error> {
+    let addr: SocketAddr = "127.0.0.1:16379".parse().unwrap();
+    let mut client = TcpStream::connect(addr).await?;
+    let (rx, mut tx) = client.split();
+    let mut total = 0;
 
-    let s = s;
-    for _ in 0..1_000_000 {
-        let mut conn = conn.clone();
-        let handle = tokio::spawn(async move {
-            redis::cmd("ENQUEUE")
-                .arg("my-queue")
-                .arg("my-value-123")
-                .query_async::<_, (i64, i64)>(&mut conn)
-                .await
-                .unwrap();
-        });
+    // redis-benchmark is pretty simple
+    // - Accept a command "cmd" and pipeline length "P"
+    // - Pre-build a buffer with "cmd" repeated "P" times
+    // - Write the output buffer entirely
+    // - Read all responses entirely
+    // - Repeat until the requested number of commands have been issued
+    let cmd = resp_array!["ENQUEUE", "my-queue", "my-value-123"];
+    let mut codec = RespCodec;
+    let mut obuf = BytesMut::new();
 
-        s.send(handle).await.expect("channel should not be closed")
+    for _ in 0..10 {
+        codec.encode(cmd.clone(), &mut obuf).unwrap();
     }
 
-    drop(s);
-    complete.await.unwrap();
+    let mut reader = FramedRead::new(rx, codec);
+
+    while total < 1_000_000 {
+        tx.write_all(&obuf).await?;
+        for _ in 0..10 {
+            reader.next().await.unwrap().unwrap();
+        }
+        total += 10;
+    }
 
     Ok(())
 }
