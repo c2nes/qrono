ef0873844bbe1f8ff4dae6dbbbda72db616bc6d6
Chris Thunes <cthunes@brewtab.com>
2022-09-27 21:02:17 -0400

    Improve timer test coverage

--- qrono/src/timer.rs
+++ qrono/src/timer.rs
@@ -1,8 +1,9 @@
-use parking_lot::{Condvar, Mutex};
-use slab::Slab;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::time::Instant;
+
+use parking_lot::{Condvar, Mutex};
+use slab::Slab;
 
 pub type ID = usize;
 pub type Deadline = Instant;
@@ -12,6 +13,9 @@ pub type Callback = Box<dyn FnOnce() + Send>;
 struct Inner {
     deadlines: Slab<Deadline>,
     callbacks: BTreeMap<(Deadline, ID), Callback>,
+
+    #[cfg(test)]
+    waiter_observed_schedule_count: Option<usize>,
 }
 
 impl Inner {
@@ -42,6 +46,10 @@ impl Inner {
             .cloned()
             .collect::<Vec<_>>();
 
+        for (_, id) in &ready_keys {
+            self.deadlines.remove(*id as usize);
+        }
+
         ready_keys
             .into_iter()
             .map(|key| self.callbacks.remove(&key).unwrap())
@@ -50,17 +58,30 @@ impl Inner {
 }
 
 // TODO: Add a way to stop the scheduler.
-// TODO: Prevent the scheduler from being started twice.
 
-#[derive(Default, Clone)]
+/// A scheduler allows callbacks to be scheduled for execution in the future.
+#[derive(Clone)]
 pub struct Scheduler {
     inner: Arc<Mutex<Inner>>,
     notify: Arc<Condvar>,
+    #[cfg(test)]
+    notify_waiting: Arc<Condvar>,
 }
 
 impl Scheduler {
     pub fn new() -> Scheduler {
-        Default::default()
+        let scheduler = Scheduler::new_unstarted();
+        scheduler.start();
+        scheduler
+    }
+
+    fn new_unstarted() -> Scheduler {
+        Scheduler {
+            inner: Arc::new(Default::default()),
+            notify: Arc::new(Default::default()),
+            #[cfg(test)]
+            notify_waiting: Arc::new(Default::default()),
+        }
     }
 
     pub fn schedule<F: FnOnce() + Send + 'static>(&self, deadline: Instant, callback: F) -> ID {
@@ -79,90 +100,64 @@ impl Scheduler {
     }
 
     pub fn start(&self) {
-        let inner = Arc::clone(&self.inner);
-        let notify = Arc::clone(&self.notify);
+        let cloned = self.clone();
+        std::thread::spawn(move || cloned.run_loop());
+    }
 
-        std::thread::spawn(move || loop {
+    fn run_loop(&self) {
+        loop {
             let ready_callbacks = {
-                let mut locked = inner.lock();
+                let mut locked = self.inner.lock();
+                #[cfg(test)]
+                {
+                    locked.waiter_observed_schedule_count = Some(locked.deadlines.len());
+                    self.notify_waiting.notify_all();
+                }
                 match locked.next_deadline() {
                     Some(deadline) => {
-                        notify.wait_until(&mut locked, deadline);
+                        self.notify.wait_until(&mut locked, deadline);
                     }
                     None => {
-                        notify.wait(&mut locked);
+                        self.notify.wait(&mut locked);
                     }
                 }
-
-                locked.take_ready_callbacks(now())
+                #[cfg(test)]
+                {
+                    locked.waiter_observed_schedule_count = None;
+                }
+                locked.take_ready_callbacks(Instant::now())
             };
 
             for callback in ready_callbacks {
                 callback();
             }
-        });
-    }
-}
-
-#[cfg(not(test))]
-fn now() -> Instant {
-    Instant::now()
-}
-
-#[cfg(test)]
-mod mock_time {
-    use once_cell::sync::Lazy;
-    use parking_lot::{Mutex, MutexGuard};
-    use std::ops::Add;
-    use std::sync;
-    use std::sync::atomic::{AtomicU64, Ordering};
-    use std::time::{Duration, Instant};
-
-    static BASE: Lazy<Instant> = Lazy::new(Instant::now);
-    static LOCK: Mutex<ClockImpl> = Mutex::new(ClockImpl {});
-    static OFFSET_NANOS: AtomicU64 = AtomicU64::new(0);
-
-    pub trait Clock {
-        fn advance(&self, offset: Duration);
-    }
-
-    struct ClockImpl {}
-
-    impl Clock for ClockImpl {
-        fn advance(&self, offset: Duration) {
-            OFFSET_NANOS.fetch_add(offset.as_nanos() as u64, Ordering::Relaxed);
         }
     }
 
-    // Tow issues:
-    //   1) Tests run concurrently and fight over the offset.
-    //   2) Using a thread local means the test thread and the scheduler thread
-    //      do not have the same time.
-
-    pub fn now() -> Instant {
-        let offset = OFFSET_NANOS.load(Ordering::Relaxed);
-        BASE.add(Duration::from_nanos(offset))
-    }
-
-    pub fn acquire() -> MutexGuard<'static, impl Clock> {
-        let clock = LOCK.lock();
-        OFFSET_NANOS.store(0, Ordering::Relaxed);
-        clock
+    #[cfg(test)]
+    fn expect_observed_schedule_count(&self, expected: usize) {
+        use std::time::Duration;
+        let deadline = Instant::now() + Duration::from_secs(5);
+        let mut locked = self.inner.lock();
+        while locked.waiter_observed_schedule_count != Some(expected) {
+            let res = self.notify_waiting.wait_until(&mut locked, deadline);
+            let actual = (*locked).waiter_observed_schedule_count;
+            assert!(
+                !res.timed_out(),
+                "expected {expected} observed schedules, actually {actual:?}"
+            );
+        }
     }
 }
 
 #[cfg(test)]
-use mock_time::now;
-
-#[cfg(test)]
 mod tests {
-    use super::mock_time::Clock;
-    use super::{now, Scheduler};
-    use log::{info, LevelFilter};
-    use qrono_channel::batch::channel;
     use std::sync::mpsc;
-    use std::sync::mpsc::{Sender, TryRecvError};
-    use std::time::{Duration, Instant};
+    use std::sync::mpsc::Sender;
+    use std::time::Duration;
+    use std::time::Instant;
+
+    use super::Scheduler;
 
     #[derive(Eq, PartialEq, Debug)]
     enum MockResult {
@@ -201,57 +196,69 @@ mod tests {
     }
 
     #[test]
-    fn cancel_drops() {
-        let clock = super::mock_time::acquire();
+    fn smoke() {
         let (tx, rx) = mpsc::channel();
         let scheduler = Scheduler::new();
-        scheduler.start();
-        scheduler.schedule(now(), MockCallback::new(&tx, "init"));
-        assert_eq!(MockResult::Call("init"), rx.recv().unwrap());
+        scheduler.schedule(
+            Instant::now() + Duration::from_millis(10),
+            MockCallback::new(&tx, "1"),
+        );
+        assert_eq!(MockResult::Call("1"), rx.recv().unwrap());
+        let locked = scheduler.inner.lock();
+        assert_eq!(0, locked.callbacks.len());
+        assert_eq!(0, locked.deadlines.len());
+    }
 
-        let id = scheduler.schedule(now() + Duration::from_secs(5), MockCallback::new(&tx, "1"));
+    #[test]
+    fn cancel_drops() {
+        let (tx, rx) = mpsc::channel();
+        let scheduler = Scheduler::new();
+        scheduler.expect_observed_schedule_count(0);
 
+        // Verify that canceling a schedule drops it.
+        let id = scheduler.schedule(
+            Instant::now() + Duration::from_secs(60),
+            MockCallback::new(&tx, "1"),
+        );
         scheduler.cancel(id);
         assert_eq!(MockResult::Drop("1"), rx.recv().unwrap())
     }
 
     #[test]
-    fn scheduled_run_on_start() {
-        let clock = super::mock_time::acquire();
-        let scheduler = Scheduler::new();
+    fn schedules_run_in_order() {
         let (tx, rx) = mpsc::channel();
+        // Create, but do not start the scheduler. This lets us install the desired schedules
+        // before allowing any to run so we can verify they run in the expected order.
+        let scheduler = Scheduler::new_unstarted();
 
         // These schedules should run in order of timestamp once the scheduler is started.
         scheduler.schedule(
-            now() + Duration::from_millis(2),
+            Instant::now() + Duration::from_millis(2),
             MockCallback::new(&tx, "2"),
         );
         scheduler.schedule(
-            now() + Duration::from_millis(1),
+            Instant::now() + Duration::from_millis(1),
             MockCallback::new(&tx, "1"),
         );
         scheduler.schedule(
-            now() + Duration::from_millis(4),
+            Instant::now() + Duration::from_millis(4),
             MockCallback::new(&tx, "4"),
         );
         scheduler.schedule(
-            now() + Duration::from_millis(6),
+            Instant::now() + Duration::from_millis(6),
             MockCallback::new(&tx, "6"),
         );
         scheduler.schedule(
-            now() + Duration::from_millis(3),
+            Instant::now() + Duration::from_millis(3),
             MockCallback::new(&tx, "3"),
         );
         scheduler.schedule(
-            now() + Duration::from_millis(5),
+            Instant::now() + Duration::from_millis(5),
             MockCallback::new(&tx, "5"),
         );
 
-        // Advance the clock past all of the deadlines and ensure all of the callbacks
-        // execute in order when we start the scheduler.
-        clock.advance(Duration::from_millis(100));
+        // Ensure all schedules run in the expected order
         scheduler.start();
-
         assert_eq!(MockResult::Call("1"), rx.recv().unwrap());
         assert_eq!(MockResult::Call("2"), rx.recv().unwrap());
         assert_eq!(MockResult::Call("3"), rx.recv().unwrap());
@@ -261,99 +268,54 @@ mod tests {
     }
 
     #[test]
-    fn cancel_middle_schedule() {
-        let clock = super::mock_time::acquire();
+    fn wake_for_new_schedule() {
         let (tx, rx) = mpsc::channel();
         let scheduler = Scheduler::new();
-        scheduler.start();
 
-        scheduler.schedule(now(), MockCallback::new(&tx, "init"));
-        assert_eq!(MockResult::Call("init"), rx.recv().unwrap());
+        // The scheduler thread should initially go to sleep
+        // after observing 0 pending schedules.
+        scheduler.expect_observed_schedule_count(0);
 
-        // These schedules should run in order of timestamp
-        scheduler.schedule(
-            now() + Duration::from_millis(2),
-            MockCallback::new(&tx, "2"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(1),
+        // Schedule a task far in future.
+        let id1 = scheduler.schedule(
+            Instant::now() + Duration::from_secs(3600),
             MockCallback::new(&tx, "1"),
         );
-        let id = scheduler.schedule(
-            now() + Duration::from_millis(4),
-            MockCallback::new(&tx, "4"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(6),
-            MockCallback::new(&tx, "6"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(3),
-            MockCallback::new(&tx, "3"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(5),
-            MockCallback::new(&tx, "5"),
-        );
 
-        // Dropped immediately
-        scheduler.cancel(id);
-        assert_eq!(MockResult::Drop("4"), rx.recv().unwrap());
+        // The scheduler thread should be woken, see the newly added schedule,
+        // and then go back to sleep waiting for the scheduled deadline to arrive.
+        scheduler.expect_observed_schedule_count(1);
 
-        // Once the clock advances, the remaining callbacks should execute in order.
-        clock.advance(Duration::from_millis(10));
-        assert_eq!(MockResult::Call("1"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("2"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("3"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("5"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("6"), rx.recv().unwrap());
-    }
-
-    #[test]
-    fn run_in_order() {
-        let clock = super::mock_time::acquire();
-        let (tx, rx) = mpsc::channel();
-        let scheduler = Scheduler::new();
-        scheduler.start();
-        scheduler.schedule(now(), MockCallback::new(&tx, "init"));
-        assert_eq!(MockResult::Call("init"), rx.recv().unwrap());
-
-        // These schedules should run in order once the clock advances.
-        scheduler.schedule(
-            now() + Duration::from_millis(2),
+        // Schedule a second task, still in the future, but sooner than the first.
+        let id2 = scheduler.schedule(
+            Instant::now() + Duration::from_secs(1800),
             MockCallback::new(&tx, "2"),
         );
+
+        // Since we have a new soonest schedule the scheduler thread should be woken,
+        // see the new soonest schedule, and go back to sleep waiting for it.
+        scheduler.expect_observed_schedule_count(2);
+
+        // Next, we schedule a third task to expire very near in the future. This too
+        // should wake the scheduler thread and the callback should be invoked quite soon.
         scheduler.schedule(
-            now() + Duration::from_millis(1),
-            MockCallback::new(&tx, "1"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(4),
-            MockCallback::new(&tx, "4"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(6),
-            MockCallback::new(&tx, "6"),
-        );
-        scheduler.schedule(
-            now() + Duration::from_millis(3),
+            Instant::now() + Duration::from_millis(10),
             MockCallback::new(&tx, "3"),
         );
-        scheduler.schedule(
-            now() + Duration::from_millis(5),
-            MockCallback::new(&tx, "5"),
+        assert_eq!(
+            MockResult::Call("3"),
+            rx.recv_timeout(Duration::from_secs(10)).unwrap()
         );
 
-        clock.advance(Duration::from_micros(3500));
-        assert_eq!(MockResult::Call("1"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("2"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("3"), rx.recv().unwrap());
-        assert_eq!(Err(TryRecvError::Empty), rx.try_recv());
+        // Cancel the first two tasks.
+        scheduler.cancel(id1);
+        assert_eq!(MockResult::Drop("1"), rx.recv().unwrap());
+        scheduler.cancel(id2);
+        assert_eq!(MockResult::Drop("2"), rx.recv().unwrap());
 
-        clock.advance(Duration::from_micros(3500));
-        assert_eq!(MockResult::Call("4"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("5"), rx.recv().unwrap());
-        assert_eq!(MockResult::Call("6"), rx.recv().unwrap());
-        assert_eq!(Err(TryRecvError::Empty), rx.try_recv());
+        // Ensure the scheduler is now empty.
+        let locked = scheduler.inner.lock();
+        assert_eq!(0, locked.callbacks.len());
+        assert_eq!(0, locked.deadlines.len());
     }
 }
