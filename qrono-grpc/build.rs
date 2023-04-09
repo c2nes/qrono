358fd64add368df1914fca2c274a6a11c025f556
Chris Thunes <cthunes@brewtab.com>
2023-04-09 14:04:19 -0400

    Keep generated files under OUT_DIR

--- qrono-grpc/build.rs
+++ qrono-grpc/build.rs
@@ -1,6 +1,7 @@
 fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
     tonic_build::configure()
-        .file_descriptor_set_path("../qrono_descriptor.pb.bin")
+        .file_descriptor_set_path(format!("{}/qrono.pb.bin", out_dir))
         .compile(&["proto/qrono.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
--- qrono-grpc/src/lib.rs
+++ qrono-grpc/src/lib.rs
@@ -1,1 +1,3 @@
 tonic::include_proto!("qrono");
+
+pub static DESCRIPTOR: &'static [u8] = include_bytes!(concat!(env!("OUT_DIR"), "/qrono.pb.bin"));
